23f4ea01c2aacd35c9b6441e4c82529889f19baa
Betty Sosterics <betty.sosterics@gmail.com>
2023-11-22 11:37:06 +0100

    Install playwright, jest

--- next.config.js
+++ next.config.js
@@ -2,15 +2,7 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true,
-    // serverActions: true,
   },
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-  // Add the domain for videos
-  // video: {
-  //   domains: ['res.cloudinary.com'],
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
