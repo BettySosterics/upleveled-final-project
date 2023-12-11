29a2a14b4300aa3eafdd4a7cd57dee0f06657e24
Betty Sosterics <betty.sosterics@gmail.com>
2023-12-11 19:17:18 +0100

    Redesign landing page to responsive

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -12,7 +12,12 @@ export default function HomePage() {
   return (
     <main>
       <header>
-        <div className="absolute top-40 text-center left-0 right-0 bg-black/50">
+        {/* <div className="top-40 text-center left-0 right-0">
+          <div className="object-fit">
+            {' '}
+            <img src="/images/appcover.jpg" alt="background" />
+          </div>
+
           <h1 className="text-9xl text-textColorNavbar font-medium drop-shadow-xl mt-5">
             B A N D I F Y
           </h1>
@@ -30,16 +35,46 @@ export default function HomePage() {
           <p className="text-3xl text-textColorNavbar font-medium drop-shadow-xl mt-10 mb-5 ">
             <Link href="/register">Register here</Link>
           </p>
-        </div>
+        </div> */}
+        <div>
+          <div
+            className="relative overflow-hidden bg-cover md:bg-cover bg-no-repeat p-12 text-center "
+            style={{
+              backgroundImage: `url("https://res.cloudinary.com/dc2znemzw/image/upload/v1702314748/gido5idgqxauudjwn5zp.jpg")`,
+              height: `100vh`,
+            }}
+          >
+            <div className="absolute bottom-5 left-0 right-0 top-0 h-screen md:h-full w-full overflow-hidden bg-fixed">
+              <div className="flex h-screen md:h-full items-center justify-center">
+                <div className="text-textColorNavbar font-medium drop-shadow-xl">
+                  <h2 className="mb-4 text-6xl md:text-7xl">B A N D I F Y</h2>
+                  <h4 className="mb-6 text-l md:text-xl">
+                    Your All-In-One Music Collaboration Platform
+                  </h4>
+                  <h5 className="mb-6 text-l md:text-xl">
+                    Got tired of switching between apps when planning your next
+                    gig?
+                  </h5>
+                  <h5 className="mb-6 text-l md:text-xl">
+                    Then let us show you what we've got!
+                  </h5>
 
-        <Image
-          src="/images/appcover.jpg"
-          alt="background image"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: '100%', height: '100%' }}
-        />
+                  <Link href="/register">
+                    <span className="bg-backgroundNavbar/75 hover:bg-buttonHover/75 rounded border-2 px-5 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal drop-shadow-xl m-5">
+                      Register here
+                    </span>
+                  </Link>
+
+                  <Link href="/login">
+                    <span className="bg-backgroundNavbar/75 hover:bg-buttonHover/75 rounded border-2 px-5 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal drop-shadow-xl m-5">
+                      Login
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </main>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout(props: Props) {
         <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <body className="bg-gradient-to-br from-background   via-buttonHover to-lynch-400 ">
+      <body>
         <nav className="fixed top-0 right-0 left-0">
           <div className="flex items-center justify-center max-w-full h-14 gap-40 bg-backgroundNavbar/75 text-textColorNavbar hover:text-hoverTextColorNavbar font-medium">
             <Link href="/">B A N D I F Y</Link>
@@ -86,6 +86,52 @@ export default async function RootLayout(props: Props) {
             )}
           </div>
         </nav>
+
+        {/* <nav className="fixed top-0 right-0 left-0">
+          <div className="flex items-center justify-center max-w-full h-14 gap-40 bg-backgroundNavbar/75 text-textColorNavbar hover:text-hoverTextColorNavbar font-medium">
+            <Link href="/">B A N D I F Y</Link>
+            {user ? (
+              <>
+                <Link href="/dashboard">Dashboard</Link>
+                <Link href="/bands">Bands</Link>
+                <Link href="/events">Events</Link>
+
+                <div className="flex items-center justify-center gap-4">
+                  <Link href={`/profile/${user.username}`}>
+                    <div className="text-lynch-900">
+                      <Image
+                        className="mx-auto rounded-full "
+                        src="/images/avatars/avatar4.svg"
+                        alt="author avatar"
+                        width={30}
+                        height={30}
+                      />
+                    </div>
+                  </Link>
+
+                  <div className="border border-white rounded-full px-5 py-1 hover:bg-buttonHover">
+                    <LogoutButton />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/register"
+                  className="border border-white rounded-full px-5 py-1  hover:bg-buttonHover"
+                >
+                  Register
+                </Link>
+                <Link
+                  href="/login"
+                  className="border border-white rounded-full px-5 py-1  hover:bg-buttonHover"
+                >
+                  Login
+                </Link>
+              </>
+            )}
+          </div>
+        </nav> */}
 
         {props.children}
       </body>
